dae5aaad0401df85d037844f62a0c6ab1237a2a5
JacobZwang <zwang.jacob@gmail.com>
2022-04-07 15:01:58 -0700

    overflow x scroll on example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,151 +20,155 @@
 
 <h1>Svelte Seats</h1>
 
-<Seats let:hovering {taken}>
-	<div class="hovering">
-		{hovering[0] ?? ''}
-		{hovering[1] ?? ''}
-	</div>
-
-	<MezzanineRect>
-		<div>
-			<div class="main-grid">
-				<div />
-				<div />
-				<MezzanineRect short>
-					<SeatsLinearLayout
-						columns={Array.from({ length: 3 }, (v, i) => i * 2 + 22)}
-						rows={['PP', 'NN', 'MM', 'LL']}
-						offsetColumns={22}
-					/>
-				</MezzanineRect>
-
-				<div />
-
-				<MezzanineRect short>
-					<SeatsLinearLayout
-						columns={Array.from({ length: 9 }, (v, i) => i * 2 + 2)}
-						rows={['PP', 'NN', 'MM', 'LL', 'KK', 'JJ', 'HH', 'GG']}
-						exclude={range(1, 8, 0, 0)}
-						reverseColumns
-					/>
-				</MezzanineRect>
-
-				<div />
-
-				<MezzanineRect short>
-					<SeatsLinearLayout
-						columns={13}
-						offsetColumns={101}
-						exclude={range(0, 13, 0, 0)}
-						rows={['PP', 'NN', 'MM', 'LL', 'KK', 'JJ', 'HH', 'GG']}
-					/>
-				</MezzanineRect>
-
-				<div />
-
-				<MezzanineRect short>
-					<SeatsLinearLayout
-						columns={Array.from({ length: 9 }, (v, i) => i * 2 + 1)}
-						rows={['PP', 'NN', 'MM', 'LL', 'KK', 'JJ', 'HH', 'GG']}
-						exclude={range(0, 7, 0, 0)}
-					/>
-				</MezzanineRect>
-
-				<div />
-
-				<MezzanineRect short>
-					<SeatsLinearLayout
-						columns={Array.from({ length: 3 }, (v, i) => i * 2 + 21)}
-						rows={['PP', 'NN', 'MM', 'LL']}
-						offsetColumns={22}
-					/>
-				</MezzanineRect>
+<div>
+	<div style="min-width: 900px;">
+		<Seats let:hovering {taken}>
+			<div class="hovering">
+				{hovering[0] ?? ''}
+				{hovering[1] ?? ''}
 			</div>
 
-			<br />
+			<MezzanineRect>
+				<div>
+					<div class="main-grid">
+						<div />
+						<div />
+						<MezzanineRect short>
+							<SeatsLinearLayout
+								columns={Array.from({ length: 3 }, (v, i) => i * 2 + 22)}
+								rows={['PP', 'NN', 'MM', 'LL']}
+								offsetColumns={22}
+							/>
+						</MezzanineRect>
 
-			<div class="main-grid">
+						<div />
+
+						<MezzanineRect short>
+							<SeatsLinearLayout
+								columns={Array.from({ length: 9 }, (v, i) => i * 2 + 2)}
+								rows={['PP', 'NN', 'MM', 'LL', 'KK', 'JJ', 'HH', 'GG']}
+								exclude={range(1, 8, 0, 0)}
+								reverseColumns
+							/>
+						</MezzanineRect>
+
+						<div />
+
+						<MezzanineRect short>
+							<SeatsLinearLayout
+								columns={13}
+								offsetColumns={101}
+								exclude={range(0, 13, 0, 0)}
+								rows={['PP', 'NN', 'MM', 'LL', 'KK', 'JJ', 'HH', 'GG']}
+							/>
+						</MezzanineRect>
+
+						<div />
+
+						<MezzanineRect short>
+							<SeatsLinearLayout
+								columns={Array.from({ length: 9 }, (v, i) => i * 2 + 1)}
+								rows={['PP', 'NN', 'MM', 'LL', 'KK', 'JJ', 'HH', 'GG']}
+								exclude={range(0, 7, 0, 0)}
+							/>
+						</MezzanineRect>
+
+						<div />
+
+						<MezzanineRect short>
+							<SeatsLinearLayout
+								columns={Array.from({ length: 3 }, (v, i) => i * 2 + 21)}
+								rows={['PP', 'NN', 'MM', 'LL']}
+								offsetColumns={22}
+							/>
+						</MezzanineRect>
+					</div>
+
+					<br />
+
+					<div class="main-grid">
+						<SeatsLinearLayout
+							columns={Array.from({ length: 14 }, (v, i) => i * 2 + 2)}
+							reverseColumns
+							rows={['FF', 'EE', 'DD', 'CC', 'BB', 'AA']}
+						/>
+
+						<div />
+						<LinearLayout columns={14} rows={6}>
+							<SeatsLinearLayout
+								columns={14}
+								offsetColumns={100}
+								rows={['FF']}
+								exclude={[
+									{
+										x: 0,
+										y: 0
+									},
+									{
+										x: 13,
+										y: 0
+									}
+								]}
+							/>
+							<SeatsLinearLayout
+								columns={14}
+								offsetColumns={101}
+								rows={['EE', 'DD', 'CC', 'BB', 'AA']}
+							/>
+						</LinearLayout>
+
+						<div />
+
+						<LinearLayout columns={14} rows={6}>
+							<SeatsLinearLayout
+								columns={11}
+								offsetColumns={1}
+								rows={['FF']}
+								incrementColums={2}
+								style="transform: translateX(14%);"
+							/>
+
+							<SeatsLinearLayout
+								offsetColumns={1}
+								columns={14}
+								rows={['EE', 'DD', 'CC', 'BB', 'AA']}
+								incrementColums={2}
+							/>
+						</LinearLayout>
+					</div>
+				</div>
+			</MezzanineRect>
+			<br />
+			<div class="main-grid" style="padding: 4px;">
+				<div />
 				<SeatsLinearLayout
-					columns={Array.from({ length: 14 }, (v, i) => i * 2 + 2)}
+					columns={13}
+					offsetColumns={2}
+					incrementColums={2}
+					rows={23}
+					alphabeticRows
 					reverseColumns
-					rows={['FF', 'EE', 'DD', 'CC', 'BB', 'AA']}
+					reverseRows
 				/>
 
 				<div />
-				<LinearLayout columns={14} rows={6}>
-					<SeatsLinearLayout
-						columns={14}
-						offsetColumns={100}
-						rows={['FF']}
-						exclude={[
-							{
-								x: 0,
-								y: 0
-							},
-							{
-								x: 13,
-								y: 0
-							}
-						]}
-					/>
-					<SeatsLinearLayout
-						columns={14}
-						offsetColumns={101}
-						rows={['EE', 'DD', 'CC', 'BB', 'AA']}
-					/>
-				</LinearLayout>
+
+				<SeatsLinearLayout columns={14} offsetColumns={101} rows={23} alphabeticRows reverseRows />
 
 				<div />
 
-				<LinearLayout columns={14} rows={6}>
-					<SeatsLinearLayout
-						columns={11}
-						offsetColumns={1}
-						rows={['FF']}
-						incrementColums={2}
-						style="transform: translateX(14%);"
-					/>
-
-					<SeatsLinearLayout
-						offsetColumns={1}
-						columns={14}
-						rows={['EE', 'DD', 'CC', 'BB', 'AA']}
-						incrementColums={2}
-					/>
-				</LinearLayout>
+				<SeatsLinearLayout
+					columns={13}
+					offsetColumns={1}
+					rows={23}
+					alphabeticRows
+					reverseColumns
+					reverseRows
+				/>
 			</div>
-		</div>
-	</MezzanineRect>
-	<br />
-	<div class="main-grid" style="padding: 8px;">
-		<div />
-		<SeatsLinearLayout
-			columns={13}
-			offsetColumns={2}
-			incrementColums={2}
-			rows={23}
-			alphabeticRows
-			reverseColumns
-			reverseRows
-		/>
-
-		<div />
-
-		<SeatsLinearLayout columns={14} offsetColumns={101} rows={23} alphabeticRows reverseRows />
-
-		<div />
-
-		<SeatsLinearLayout
-			columns={13}
-			offsetColumns={1}
-			rows={23}
-			alphabeticRows
-			reverseColumns
-			reverseRows
-		/>
+		</Seats>
 	</div>
-</Seats>
+</div>
 
 <style>
 	h1 {
